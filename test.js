78c6188794739e686019fb03ac38d9e0b2bfd050
Artem Tyurin <artem.tyurin@gmail.com>
2016-12-31 14:35:40 +0100

    Test

---
+++ test.js
@@ -0,0 +1,27 @@
+import test from 'ava';
+
+const ah = require('./');
+
+const ahMilkUrl = 'https://www.ah.nl/producten/product/wi33693/ah-halfvolle-melk';
+
+const ahMilkNutritionFacts = {
+  Energie: '200 kJ (48 kcal)',
+  Vet: '1,5 g',
+  'Waarvan verzadigd': '1 g',
+  'Waarvan enkelvoudig onverzadigd': '0,5 g',
+  'Waarvan meervoudig onverzadigd': '0 g',
+  Koolhydraten: '4,5 g',
+  'Waarvan suikers': '4,5 g',
+  Voedingsvezel: '0 g',
+  Eiwitten: '3,5 g',
+  Zout: '0,1 g',
+  'Riboflavine/vitamine B2': '0,18 mg',
+  'Vitamine B12': '0,47 µg',
+  Calcium: '126 mg'
+};
+
+test('bar', async t => {
+  const nutritionFacts = await ah.getNutritionFacts(ahMilkUrl);
+
+  t.deepEqual(nutritionFacts, ahMilkNutritionFacts);
+});
